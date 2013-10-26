90dceba691e234f586011d511f3372167c7e52f6
Salvador Sápia <salvador@ssapia.net>
2013-10-26 11:08:53 -0200

    corrige bug numero disponivel

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,11 +1,43 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SessaoTest {
 
+	static int x = 1; 
+	int y = 0; 
 
+	@BeforeClass
+	public static void testBeforeClass() {
+		x = 1;
+	}
+
+	@Before
+	public void testBefore() {
+		y = 1;
+	}
+	
+	@Test
+	public void xDeveSer1() throws Exception {
+        Assert.assertEquals(x, 1);
+	}
+
+	@Test
+	public void yDeveSer1() throws Exception {
+		Assert.assertEquals(y, 1);
+	}
+	
+	@Test
+	public void deveVender1ingressoSeHa1vagas() throws Exception {
+		Sessao sessao = new Sessao();
+        sessao.setTotalIngressos(1);
+
+        Assert.assertTrue(sessao.podeReservar(1));
+	}
+	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
 		Sessao sessao = new Sessao();
